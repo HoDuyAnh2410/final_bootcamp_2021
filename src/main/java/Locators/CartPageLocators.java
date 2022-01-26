89064042ee3669af1112360ae933e1b92c5a6986
hoduyanh <nhuanhoangminh2410@gmail.com>
2022-01-26 18:16:02 +0700

    create shopping cart icon locator

--- src/main/java/Locators/CartPageLocators.java
+++ src/main/java/Locators/CartPageLocators.java
@@ -1,5 +1,10 @@
 package Locators;
 
+import org.openqa.selenium.By;
+
 public class CartPageLocators {
     //elements of shoppingCartIcon, removeButton, checkOutButton, continueShoppingButton
+    public static By img_shoppingCartIcon(By by) {
+        return By.xpath("//a[@class='shopping_cart_link']");
+    }
 }
